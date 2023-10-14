453b17da879beb3d51924ec7f570337164423920
Kepler <126917322+Kepler54@users.noreply.github.com>
2023-10-14 20:05:51 +0200

    1. Beginning (aiogram-3.x )

--- keyboards.py
+++ keyboards.py
@@ -1,8 +1,1 @@
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-
-
-class KeyboardClose:
-    """Close keyboard"""
-    inline_keyboard = InlineKeyboardMarkup(row_width=3)
-    cancel_button = InlineKeyboardButton(text="Закрыть", callback_data="close")
-    inline_keyboard.add(cancel_button)
+pass
